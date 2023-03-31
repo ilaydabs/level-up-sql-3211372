8a97d6fda8a6f83e5f815e4e2a012a6f8dc3837a
ilayda <79340608+ilaydabs@users.noreply.github.com>
2023-03-31 13:37:43 +0000

    "restaurant challenge" is solved

--- 01_restaurant/challenges/01_07_attendevent_ch.sql
+++ 01_restaurant/challenges/01_07_attendevent_ch.sql
@@ -2,3 +2,18 @@
 
 -- The customer 'atapley2j@kinetecoinc.com' will be in
 -- attendance, and will bring 3 friends.
+
+
+
+--SELECT * FROM Customers WHERE Email = 'atapley2j@kinetecoinc.com';
+
+--INSERT INTO AnniversaryAttendees (CustomerID, PartSize) VALUES (92, 3);
+
+--SELECT * FROM AnniversaryAttendees;
+
+
+
+INSERT INTO AnniversaryAttendees (CustomerID, PartSize) 
+VALUES ((SELECT CustomerID FROM Customers WHERE Email = 'atapley2j@kinetecoinc.com'),4);
+
+SELECT * FROM AnniversaryAttendees;
